6c799fee7641cd2f8a4a9a257070901ed504ae3e
Nikhom Phutthawithee <mr_nikom@yahoo.com>
2019-04-14 15:23:38 +0700

    2nd Change

--- src/App.js
+++ src/App.js
@@ -15,21 +15,32 @@ class App extends Component {
   
   componentDidMount(){
     axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then(res=>this.setState({todos:res.data}))
+      .then(res=>this.setState({
+        todos:res.data
+      }))
   }
 
   markComplete=(id)=>{
-    this.setState({todos:this.state.todos.map(todo=>{
-      if(todo.id===id){
-        todo.completed = !todo.completed
-      }
-      return todo;
-    })});
+    this.setState({
+      todos:this.state.todos.map(
+        todo=>{
+          if(todo.id===id){
+          todo.completed = !todo.completed
+          }
+          return todo;
+        }
+      )
+    });
   }
 
   delTodo=(id)=>{
     axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res=>this.setState({todos:[...this.state.todos.filter(todo=>todo.id!==id)]}));
+      .then(res=>this.setState({
+        todos:[...this.state.todos.filter(
+          todo=>todo.id!==id
+        )]
+      })
+    );
   }
 
   addTodo = (title) => {
@@ -39,8 +50,10 @@ class App extends Component {
     })
       .then(res => {
         res.data.id=uuid();
-        this.setState({ todos: [...this.state.todos, res.data] })
-        });
+        this.setState({ 
+          todos: [...this.state.todos, res.data] 
+        })
+      });
   }
 
   render() {
@@ -55,7 +68,8 @@ class App extends Component {
                   addTodo={this.addTodo}
                 />
                 <Todos 
-                  todos={this.state.todos} markComplete={this.markComplete}
+                  todos={this.state.todos} 
+                  markComplete={this.markComplete}
                   delTodo={this.delTodo}
                 />
               </React.Fragment>
